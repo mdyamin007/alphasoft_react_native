ed640045f6254d0f34026791f117a0248a612ef6
Md. Yamin <mohammadyamin80@gmail.com>
2024-09-18 21:31:43 +0600

    v1.0.1

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router'
 const _layout = () => {
   return (
     <>
-    <Stack screenOptions={{headerShown: true}}>
+    <Stack initialRouteName='index' screenOptions={{headerShown: true}}>
     <Stack.Screen name='index' options={{headerShown: false}}/>
     <Stack.Screen name='scan' options={{headerShown: true, title: "Scan QR/Barcode"}}/>
     </Stack>
